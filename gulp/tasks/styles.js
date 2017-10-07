71cd7506a89ef1222db470995092205bbb464e83
SerDN <sergiodanielnaveira@gmail.com>
2017-10-07 07:52:06 -0300

    Completed large hero block and intro content block.

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,18 @@
+var gulp = require("gulp");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var cssvars = require("postcss-simple-vars");
+var nested = require("postcss-nested");
+var cssImport = require("postcss-import");
+var mixins = require("postcss-mixins");
+
+
+gulp.task("styles", function(){
+    return gulp.src("app/assets/styles/styles.css") // elegimos el archivo origen
+        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer])) // hacemos que antes de copiar el archivo, lo convierta a un archivo css válido. En este caso, que permita importar archivos parciales css en uno solo, que convierta las variables (cssvars), convierta el css anidado a css válido, y use autoprefixer (agregue las etiquetas de compatibilidad de cada navegador).
+        .on("error", function(errorInfo){ // si ocurre un evento de tipo error, se ejecutará la función.
+            console.log(errorInfo.toString());
+            this.emit("end");
+        })
+        .pipe(gulp.dest("app/temp/styles")); // elegimos donde queremos la copia.
+});
---
+++ gulp/tasks/watch.js
@@ -0,0 +1,27 @@
+var gulp = require("gulp");
+var watch = require("gulp-watch");
+var browserSync = require("browser-sync").create();
+
+gulp.task("watch", function(){
+
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "app" // le pasamos la ruta del index.html
+        }
+    });
+
+    watch("app/index.html", function(){ // le decimos que escuche por cambios en el archivo index.html, y que cuando haya un cambio ejecute el método reload (recargará la pagina automáticamente).
+        browserSync.reload();
+    });
+
+    watch("app/assets/styles/**/*.css", function(){ // cada vez que haya un cambio en un archivo .css hará saltar la tarea cssInject.
+        gulp.start("cssInject");
+    });
+});
+
+// hacemos que autorefresque la pagina cada vez que haya un cambio en un archivo .css
+gulp.task("cssInject", ["styles"], function(){ // antes de correr la tarea cssInject, primero correrá la tarea styles.
+    return gulp.src("app/temp/styles/styles.css")
+        .pipe(browserSync.stream());
+});
